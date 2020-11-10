c079b949e26b934e605b1234efacb9b9cc076adb
Josh <critesjosh@gmail.com>
2020-11-10 18:43:31 -0500

    Update celo_deploy.js

--- celo_deploy.js
+++ celo_deploy.js
@@ -1,1 +1,22 @@
+const Kit = require('@celo/contractkit')
+const HelloWorld = require('./build/contracts/HelloWorld.json')
 
+const kit = Kit.newKit('https://alfajores-forno.celo-testnet.org')
+const getAccount = require('./getAccount').getAccount
+
+async function awaitWrapper(){
+    let account = await getAccount()
+    console.log(account.address)
+    
+//     kit.addAccount(account.privateKey) // this account must have a CELO balance to pay transaction fees
+
+//     let tx = await kit.sendTransaction({
+//         from: account.address,
+//         data: HelloWorld.bytecode
+//     })
+
+//     const receipt = await tx.waitReceipt()
+//     console.log(receipt)
+}
+
+awaitWrapper()
