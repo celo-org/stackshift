c3b5a8fa6ba74a237d02e32a33db2c25dbbce2ab
Josh <critesjosh@gmail.com>
2021-01-20 15:47:42 -0500

    Update celo_deploy.js

--- celo_deploy.js
+++ celo_deploy.js
@@ -1,8 +1,9 @@
-const Kit = require('@celo/contractkit')
-const HelloWorld = require('./build/contracts/HelloWorld.json')
-
-const kit = Kit.newKit('https://alfajores-forno.celo-testnet.org')
+const Web3 = require('web3')
+const ContractKit = require('@celo/contractkit')
+const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
+const kit = ContractKit.newKitFromWeb3(web3)
 const getAccount = require('./getAccount').getAccount
+const HelloWorld = require('./build/contracts/HelloWorld.json')
 
 async function awaitWrapper(){
     let account = await getAccount()
