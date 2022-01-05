cb69a0af3d5799373a1c0586da43f4382d45a589
Josh Crites <critesjosh@gmail.com>
2022-01-05 10:55:46 -0500

    update custom deploy

--- celo_deploy.js
+++ celo_deploy.js
@@ -1,25 +1,26 @@
 const Web3 = require('web3')
 const ContractKit = require('@celo/contractkit')
 const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
+const privateKeyToAddress = require('@celo/utils/lib/address').privateKeyToAddress
 const kit = ContractKit.newKitFromWeb3(web3)
 require('dotenv').config()
-// const HelloWorld = require('./build/contracts/HelloWorld.json')
+const HelloWorld = require('./build/contracts/HelloWorld.json')
 
-async function awaitWrapper(){
-    
+async function awaitWrapper(){    
+    kit.connection.addAccount(process.env.PRIVATE_KEY) // this account must have a CELO balance to pay transaction fees
+
     // This account must have a CELO balance to pay tx fees 
     // get some testnet funds at https://celo.org/build/faucet
-    console.log(account.address)
-    
-//     kit.connection.addAccount(process.env.PRIVATE_KEY) // this account must have a CELO balance to pay transaction fees
+    const address = privateKeyToAddress(process.env.PRIVATE_KEY)
+    console.log(address)
 
-//     let tx = await kit.connection.sendTransaction({
-//         from: account.address,
-//         data: HelloWorld.bytecode
-//     })
+    let tx = await kit.connection.sendTransaction({
+        from: address,
+        data: HelloWorld.bytecode
+    })
 
-//     const receipt = await tx.waitReceipt()
-//     console.log(receipt)
+    const receipt = await tx.waitReceipt()
+    console.log(receipt)
 }
 
 awaitWrapper()
