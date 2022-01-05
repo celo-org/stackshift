01dda38c357922cb94f7c3f5bd47c62963c9a169
Josh Crites <critesjosh@gmail.com>
2022-01-05 11:25:28 -0500

    fix indent

--- truffle-config.js
+++ truffle-config.js
@@ -9,14 +9,14 @@
  * truffleframework.com/docs/advanced/configuration
  */
 
- const ContractKit = require('@celo/contractkit')
- const Web3 = require('web3')
- require('dotenv').config()
- 
- const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
- const kit = ContractKit.newKitFromWeb3(web3)
- 
- kit.connection.addAccount(process.env.PRIVATE_KEY)
+const ContractKit = require('@celo/contractkit')
+const Web3 = require('web3')
+require('dotenv').config()
+
+const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
+const kit = ContractKit.newKitFromWeb3(web3)
+
+kit.connection.addAccount(process.env.PRIVATE_KEY)
 
 module.exports = {
   /**
