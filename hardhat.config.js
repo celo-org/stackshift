4cf84504d372faecdb56e115c7da3085aff1b757
Victor Ezea <victorvikson@gmail.com>
2023-03-29 15:43:07 +0100

    SMart COntract Updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,48 @@
 require("@nomicfoundation/hardhat-toolbox");
+const env = require("dotenv").config({ path: ".env" });
+console.log(env);
+
+const DEVCHAIN_MNEMONIC =
+  "concert load couple harbor equip island argue ramp clarify fence smart topic";
+
+const defaultNetwork = "alfajores";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork,
+
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic: DEVCHAIN_MNEMONIC,
+      },
+    },
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 44787,
+    },
+    celo: {
+      url: "https://forno.celo.org",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 42220,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      alfajores: process.env.CELOSCAN_API_KEY,
+      celo: process.env.CELOSCAN_API_KEY,
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  typechain: {
+    outDir: "types",
+    target: "web3-v1",
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ["externalArtifacts/*.json"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+  },
 };
