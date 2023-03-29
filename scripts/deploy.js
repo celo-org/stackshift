a88c86bcc32d5de20a0f3b2e10d5754bbe3050b6
Victor Ezea <victorvikson@gmail.com>
2023-03-29 05:30:50 +0100

    updated contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,34 +4,15 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
   const Splitter = await ethers.getContractFactory("Splitter");
   const split = await Splitter.deploy("Hello, Hardhat!");
 
-  await split.deployed({
-    data: Splitter.bytecode,
-    arguments: [friend1, friend2],
-  });
+  await split.deployed({});
 
   console.log("Splitter:", split.address);
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
