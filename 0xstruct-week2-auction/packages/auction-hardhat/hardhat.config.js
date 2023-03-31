4f4bd10be30a0aaab42bf93c3e47fc2beb9447e2
0xstruct <0xstruct@gmail.com>
2023-03-31 19:51:54 +0630

    basic contract code and test for contract
    basic contract code and test for contract
    
    issue: testing on Alfajores directly has problem with inconsistent lagging data updates

--- 0xstruct-week2-auction/packages/auction-hardhat/hardhat.config.js
+++ 0xstruct-week2-auction/packages/auction-hardhat/hardhat.config.js
@@ -2,6 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 require("hardhat-deploy");
 
+const defaultNetwork = "alfajores";
+const mnemonicPath = "m/44'/52752'/0'/0"; // derivation path used by Celo
+
+// This is the mnemonic used by celo-devchain
+const DEVCHAIN_MNEMONIC =
+    "salad million smart birth jazz life churn blush dish link blouse debate hockey remember search hungry embody bread unit basket risk find office trash";
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -20,17 +27,28 @@ module.exports = {
   defaultNetwork: "alfajores",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:7545",
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic: DEVCHAIN_MNEMONIC,
+      },
     },
     alfajores: {
       url: "https://alfajores-forno.celo-testnet.org",
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: {
+        mnemonic: DEVCHAIN_MNEMONIC,
+      },
       chainId: 44787,
     },
     celo: {
       url: "https://forno.celo.org",
       accounts: [process.env.PRIVATE_KEY],
       chainId: 42220,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      alfajores: process.env.CELOSCAN_API_KEY,
+      celo: process.env.CELOSCAN_API_KEY,
     },
   },
   solidity: {
