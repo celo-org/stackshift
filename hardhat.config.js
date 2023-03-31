bea001a19f4900a717aa173e469003544d54d4e7
Victor Ezea <victorvikson@gmail.com>
2023-03-31 02:35:00 +0100

    done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
 const env = require("dotenv").config({ path: ".env" });
-console.log(env);
 
 const DEVCHAIN_MNEMONIC =
   "concert load couple harbor equip island argue ramp clarify fence smart topic";
@@ -11,7 +10,6 @@ const defaultNetwork = "alfajores";
 module.exports = {
   solidity: "0.8.18",
   defaultNetwork,
-
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
