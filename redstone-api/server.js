d2cdb9d4091e034465de765190b2fe939bc91a4b
Jimii <jimmyimpulse2@gmail.com>
2023-04-23 16:41:49 +0300

    chore

--- redstone-api/server.js
+++ redstone-api/server.js
@@ -17,18 +17,11 @@ const requestOptions = {
 };
 
 app.get('/prices', async (req, res) => {
-    // fetch("https://api.apilayer.com/fixer/convert?to=kes&from=usd&amount=1", requestOptions)
-    //     .then(response => response.text())
-    //     .then(result => console.log(result))
-    //     .catch(error => console.log('error', error));
-
         const resp = await fetch("https://api.apilayer.com/fixer/convert?to=kes&from=usd&amount=1", requestOptions);
         const json = await resp.json();
         console.log("resp", json);
 
         res.send(json);
-        // res.json(await resp.json());
-        // res.send("nothing")
 })
 
 const port = 3040;
