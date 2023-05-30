79c804b0ad88f24fb426b75b1975d110dff42bce
chinwizu <chinwianthony@gmail.com>
2023-05-30 16:09:19 +0100

    week5-done

---
+++ refi-dapp-done/packages/react-app/pages/index.tsx
@@ -0,0 +1,16 @@
+import CarbonRedeem from "../components/CarbonRedeem";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="h1">
+        <p 
+        className="text-center text-[45px] font-semibold"
+        >Save the environment by redeeming TCO tokens</p>
+        <CarbonRedeem/>
+
+      </div>
+      
+    </div>
+  )
+}
