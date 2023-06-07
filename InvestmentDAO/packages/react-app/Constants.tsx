5a43c7ba0ee40f9941f3388ab28de8cd5bbea2d6
Glory Agatevure <agatevureglory@gmail.com>
2023-06-07 04:35:27 +0100

    update smart contract addresses and abis

--- InvestmentDAO/packages/react-app/Constants.tsx
+++ InvestmentDAO/packages/react-app/Constants.tsx
@@ -1,12 +1,15 @@
 import ContractABI from './Dao.json'
 import TokenABI from './Token.json'
+import NFTABI from './nft.json'
 
-const CONTRACT_ADDRESS = '0xA6d3E7d9cf98F7458124415c8114FE059886edbE'
+const CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 const CONTRACT_ABI = ContractABI
 
 const TOKEN_ADDRESS = "0xD590c2ED8aC1181a20680D114ab197504b445b9F"
 const TOKEN_ABI = TokenABI
 
+const NFT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+const NFT_ABI = NFTABI
 const OWNER = "0x65645067615CAFCc37AB63ADEe2bc1231Fb257f3"
 
 export {
