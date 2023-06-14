3a7afa15427e4d516cb0f7d77a7ca1a1494d9ba2
chinwizu <chinwianthony@gmail.com>
2023-06-14 13:18:08 +0100

    week8

--- refi-dapp-done/packages/react-app/apollo-client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/toucanprotocol/alfajores",
-  cache: new InMemoryCache(),
-});
-
-export default client;
